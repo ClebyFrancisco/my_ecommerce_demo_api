8eae63c6d19e94dd38bdd1adcb8fb1a89e9e01e5
Cleby Francisco <clebysilva5@gmail.com>
2025-02-25 12:14:49 -0300

    feat: list products

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+  constructor(private productsService: ProductsService) {}
+
+  @Post('create')
+  async createProduct(
+    @Body('name') name: string,
+    @Body('description') description: string,
+    @Body('price') price: number,
+    @Body('imageUrl') imageUrl: string,
+  ) {
+    return this.productsService.createProduct(
+      name,
+      description,
+      price,
+      imageUrl,
+    );
+  }
 
   @Get()
   async getAll() {
--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -5,6 +5,26 @@ import { PrismaService } from 'src/prisma/prisma.service';
 export class ProductsService {
   constructor(private prisma: PrismaService) {}
 
+  async createProduct(
+    name: string,
+    description: string,
+    price: number,
+    imageUrl: string,
+  ) {
+    const newProduct = await this.prisma.product.create({
+      data: {
+        name,
+        description,
+        price,
+        imageUrl,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
+
+    return newProduct;
+  }
+
   async getAll() {
     return this.prisma.product.findMany();
   }
